b9706b0c4761967c45b3151d272fa543e793f92e
Aliaksei Rudak <lexarudak@gmail.com>
2023-05-06 15:51:33 +0600

    feat: add empty pages

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -1,9 +1,9 @@
 import styles from './page.module.scss';
 
-export default function LoginPage(): JSX.Element {
+export default function PlaygroundPage(): JSX.Element {
   return (
     <section className={styles.page}>
-      <h1>Login page</h1>
+      <h1>Playground page</h1>
     </section>
   );
 }
