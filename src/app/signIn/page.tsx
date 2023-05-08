0d642ef08bbb0fc97b4d1c0d01452263104b8748
Yu Mentsel <yu.mentsel@gmail.com>
2023-05-08 22:48:22 +0300

    feat: add redux

--- src/app/signIn/page.tsx
+++ src/app/signIn/page.tsx
@@ -1,9 +1,21 @@
+// 'use client';
+
+// import { setAuthState } from '@/redux/slices/authSlice';
+// import { useAppDispatch, useAppSelector } from '@/redux/hooks';
 import styles from './page.module.scss';
 
 export default function SignIn(): JSX.Element {
+  // const authState = useAppSelector((state) => state.authSlice.authState);
+  // const dispatch = useAppDispatch();
+  // console.log(authState);
+
   return (
     <section className={styles.page}>
-      <h1>Sign in page</h1>
+      <h1
+      // onClick={() => (authState ? dispatch(setAuthState(false)) : dispatch(setAuthState(true)))}
+      >
+        Sign in page
+      </h1>
     </section>
   );
 }
