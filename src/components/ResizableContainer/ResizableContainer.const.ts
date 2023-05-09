11bafb99b4dcfbf9952837e69fa3f1aedcbe6d0d
Aliaksei Rudak <lexarudak@gmail.com>
2023-05-09 08:12:34 +0600

    feat: create resixeble container

---
+++ src/components/ResizableContainer/ResizableContainer.const.ts
@@ -0,0 +1,7 @@
+export const LS_PG_SIZE = 'playgroundSize';
+
+export const DEFAULT_PG_SIZE = ['33%', '34%', '33%'];
+
+export const MIN_PANE_SIZE = 250;
+
+export const HIDE_LEFT_PANE_SIZE = 40;
--- src/components/ResizableContainer/ResizableContainer.tsx
+++ src/components/ResizableContainer/ResizableContainer.tsx
@@ -1,38 +1,63 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
 'use client';
-import styles from './page.module.scss';
-import React, { useState } from 'react';
+import styles from './ResizableContainer.module.scss';
+import React, { useEffect, useRef, useState } from 'react';
 import SplitPane, { Pane } from 'split-pane-react';
 import 'split-pane-react/esm/themes/default.css';
-import { ButtonStyle, MainButton } from '../buttons';
+import { RoundButton } from '../buttons';
+import {
+  DEFAULT_PG_SIZE,
+  HIDE_LEFT_PANE_SIZE,
+  LS_PG_SIZE,
+  MIN_PANE_SIZE,
+} from './ResizableContainer.const';
+import { Split } from './ResizableContainer.enum';
+import classNames from 'classnames';
 
 export default function ResizableContainer(): JSX.Element {
-  const [sizes, setSizes] = useState(['33%', '33%', '33%']);
+  const lsState = localStorage.getItem(LS_PG_SIZE);
+  const [sizes, setSizes] = useState(lsState ? JSON.parse(lsState) : DEFAULT_PG_SIZE);
+  const currentSizeRef = useRef(sizes);
+  const [leftPaneSize] = sizes;
 
-  const layoutCSS = {
-    height: '100%',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+  const setLs = (): void => {
+    localStorage.setItem(LS_PG_SIZE, JSON.stringify(currentSizeRef.current));
   };
 
-  const aaa = (): void => {
-    setSizes([0]);
+  useEffect((): void => {
+    currentSizeRef.current = sizes;
+  }, [sizes]);
+
+  useEffect((): (() => void) => {
+    window.addEventListener('beforeunload', setLs);
+    return (): void => {
+      window.removeEventListener('beforeunload', setLs);
+      setLs();
+    };
+  }, []);
+
+  const toggleLeftPane = (): void => {
+    setSizes([HIDE_LEFT_PANE_SIZE]);
   };
 
   return (
-    <SplitPane split="vertical" sizes={sizes} onChange={setSizes}>
-      <Pane minSize={250}>
-        <div style={{ ...layoutCSS }}>
-          <MainButton style={ButtonStyle.active} disabled={false} action={aaa}></MainButton>
+    <SplitPane split={Split.vertical} sizes={sizes} onChange={setSizes}>
+      <Pane minSize={MIN_PANE_SIZE}>
+        <div className={classNames(styles.pane, styles.paneLeft)}>
+          <div className={styles.hideButton}>
+            <RoundButton action={toggleLeftPane}>
+              {leftPaneSize > HIDE_LEFT_PANE_SIZE ? '<' : '>'}
+            </RoundButton>
+          </div>
+          <div>Documentation</div>
         </div>
       </Pane>
-      <Pane minSize={250}>
-        <div style={{ ...layoutCSS, background: '#d5d7d9' }}>pane2</div>
+      <Pane minSize={MIN_PANE_SIZE}>
+        <div className={classNames(styles.pane, styles.paneCenter)}>Operation</div>
       </Pane>
-      <Pane minSize={250}>
-        <div style={{ ...layoutCSS }}>pane1</div>
+      <Pane minSize={MIN_PANE_SIZE}>
+        <div className={styles.pane}>Response</div>
       </Pane>
     </SplitPane>
   );
