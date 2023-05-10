ea359c91eb5dff60926e44d13c97a8f732c74f3b
neemkashu <sparjinkust@mail.ru>
2023-05-10 17:59:30 +0300

    feat: add password validation

--- src/components/auth/forms/Register/RegisterForm.tsx
+++ src/components/auth/forms/Register/RegisterForm.tsx
@@ -1,52 +1,111 @@
 'use client';
 
-import { FormEventHandler, useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import styles from './RegisterForm.module.scss';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { firebaseAuth, registerWithEmailAndPassword } from '@/firebase';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { firebaseAuth } from '@/firebase';
 import { useRouter } from 'next/navigation';
 import { PageList } from '@/common';
+import { useForm } from 'react-hook-form';
+import { RegisterData } from '@/components/auth/forms/forms.type';
+import { AuthInputNames } from '@/components/auth/forms/forms.enum';
+import {
+  MAX_EMAIL_LENGTH,
+  EMAIL_REGEX,
+  ONE_LETTER,
+  ONE_DIGIT,
+  MIN_PASSWORD_LENGTH,
+  ONE_SPECIAL_CHAR,
+} from '@/components/auth/forms/forms.const';
 
 export const RegisterForm = (): JSX.Element => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [name] = useState('kT');
-  const [user, loading] = useAuthState(firebaseAuth);
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(firebaseAuth);
   const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegisterData>({ mode: 'onSubmit', reValidateMode: 'onBlur' });
 
-  const handleRegister: FormEventHandler<HTMLFormElement> = (event): void => {
-    event.preventDefault();
-    if (!name) alert('Please enter name');
-    registerWithEmailAndPassword(name, email, password);
-  };
   useEffect((): void => {
     if (loading) return;
     if (user) router.push(PageList.playground);
   }, [user, loading, router]);
 
+  const handleRegister = async ({ email, password }: RegisterData): Promise<void> => {
+    const user = await createUserWithEmailAndPassword(email, password);
+  };
+
   return (
-    <form className={styles.form} onSubmit={handleRegister}>
-      <label htmlFor="email">Email</label>
-      <input
-        className={styles.input}
-        type="email"
-        name="email"
-        value={email}
-        onChange={(e): void => setEmail(e.target.value)}
-        placeholder="Enter email"
-      />
-      <label htmlFor="password">Password</label>
-      <input
-        className={styles.input}
-        type="password"
-        name="password"
-        value={password}
-        onChange={(e): void => setPassword(e.target.value)}
-        placeholder="Enter password"
-      />
-      <button className={styles.button} type="submit">
-        Create account
-      </button>
-    </form>
+    <>
+      {error && <p>{error?.message}</p>}
+      <form className={styles.form} onSubmit={handleSubmit(handleRegister)}>
+        <div>
+          <label htmlFor={AuthInputNames.EMAIL}>Email</label>
+          {errors.email && <span className={styles.formError}>{errors.email.message}</span>}
+        </div>
+        <input
+          className={styles.input}
+          type="email"
+          placeholder="Enter email"
+          {...register(AuthInputNames.EMAIL, {
+            required: 'Please enter email',
+            maxLength: {
+              value: MAX_EMAIL_LENGTH,
+              message: `Email exceeded ${MAX_EMAIL_LENGTH} symbols`,
+            },
+            pattern: {
+              value: EMAIL_REGEX,
+              message: 'Invalid email',
+            },
+          })}
+        />
+        <div>
+          <label htmlFor={AuthInputNames.PASSWORD}>Password</label>
+          {errors.password && <span className={styles.formError}>{errors.password.message}</span>}
+        </div>
+        <input
+          className={styles.input}
+          type="password"
+          placeholder="Enter password"
+          {...register(AuthInputNames.PASSWORD, {
+            required: 'Please enter password',
+            minLength: {
+              value: MIN_PASSWORD_LENGTH,
+              message: 'At least 8 characters in length',
+            },
+            validate: {
+              oneLetter: (value): boolean | string =>
+                ONE_LETTER.test(value) || 'At least one letter',
+              oneDigit: (value): boolean | string => ONE_DIGIT.test(value) || 'At least one digit',
+              oneSpecialChar: (value): boolean | string =>
+                ONE_SPECIAL_CHAR.test(value) || 'At least one special character',
+            },
+          })}
+        />
+        <div>
+          <label htmlFor={AuthInputNames.REPEAT_PASSWORD}>Confirm</label>
+          {errors.repeatPassword && (
+            <span className={styles.formError}>{errors.repeatPassword.message}</span>
+          )}
+        </div>
+        <input
+          className={styles.input}
+          type="password"
+          placeholder="Repeat password"
+          {...register(AuthInputNames.REPEAT_PASSWORD, {
+            required: 'Please confirm your password',
+            validate: {
+              equalPassword: (value): boolean | string =>
+                true || 'Password must contain at least one letter',
+            },
+          })}
+        />
+        <button className={styles.button} type="submit">
+          Create account
+        </button>
+      </form>
+    </>
   );
 };
--- src/components/auth/forms/forms.const.ts
+++ src/components/auth/forms/forms.const.ts
@@ -1,3 +1,6 @@
 export const EMAIL_REGEX = /^[\w.%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 export const MAX_EMAIL_LENGTH = 320;
 export const MIN_PASSWORD_LENGTH = 8;
+export const ONE_LETTER = /.*[a-zA-Z].*/;
+export const ONE_DIGIT = /.*\d.*/;
+export const ONE_SPECIAL_CHAR = /.*[!@#$%^&*().,?":{}|<>`/ ].*/;
