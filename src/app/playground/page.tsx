bde48e40cf24a7be1ab2284c74f3a91fe96cb989
Aliaksei Rudak <lexarudak@gmail.com>
2023-05-11 14:00:25 +0600

    feat: add hook to switch mobile and desctop playground veiw

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -1,10 +1,13 @@
+/* eslint-disable no-console */
+'use client';
+import { useWidthState } from '@/app/playground/page.hook';
 import { ResizableContainer } from '@/components/ResizableContainer/ResizableContainer';
 import styles from './page.module.scss';
 
 export default function PlaygroundPage(): JSX.Element {
+  const isMobileView = useWidthState();
+
   return (
-    <div className={styles.playground}>
-      <ResizableContainer />
-    </div>
+    <div className={styles.playground}>{isMobileView ? 'mobile' : <ResizableContainer />}</div>
   );
 }
