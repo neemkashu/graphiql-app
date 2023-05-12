d82ba69c382ea44a4a8bd1279781117392e90c8e
Aliaksei Rudak <lexarudak@gmail.com>
2023-05-12 18:16:52 +0600

    fix: solve ts error

--- src/common/hook.ts
+++ src/common/hook.ts
@@ -4,7 +4,7 @@ import { usePathname } from 'next/navigation';
 
 export const usePathWithLocale = (pagePath: PageList[]): string[] => {
   const pathName = usePathname();
-  const locale = pathName.slice(1, 3);
+  const locale = pathName ? pathName.slice(1, 3) : BASIC_LANGUAGE;
   const outputLocale = ALL_LANGUAGES.includes(locale) ? locale : BASIC_LANGUAGE;
   return pagePath.map((page): string => `/${outputLocale}${page}`);
 };
