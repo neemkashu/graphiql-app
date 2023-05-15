f961d5fa6372b566c657e7f38b13748baae9a754
Aliaksei Rudak <lexarudak@gmail.com>
2023-05-15 11:09:21 +0600

    feat: save redux state to local storage

--- src/redux/provider.tsx
+++ src/redux/provider.tsx
@@ -2,7 +2,13 @@
 
 import { store } from './store';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor } from '@/redux';
 
 export const Providers = ({ children }: { children: React.ReactNode }): JSX.Element => {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>{children}</PersistGate>
+    </Provider>
+  );
 };
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,41 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from '@reduxjs/toolkit';
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import authSlice from './auth/auth.slice';
 import playgroundSlice from './playground/playground.slice';
 
-export const store = configureStore({
-  reducer: {
-    authSlice,
-    playgroundSlice,
-  },
-  devTools: process.env.NODE_ENV !== 'production',
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const rootReducer = combineReducers({
+  authSlice,
+  playgroundSlice,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
