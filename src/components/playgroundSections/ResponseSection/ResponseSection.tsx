e7fa007710f82974808c1a990f792f774173df7a
Aliaksei Rudak <lexarudak@gmail.com>
2023-05-15 08:01:39 +0600

    feat: link response section with redux

--- src/components/playgroundSections/ResponseSection/ResponseSection.tsx
+++ src/components/playgroundSections/ResponseSection/ResponseSection.tsx
@@ -1,14 +1,24 @@
 'use client';
-import styles from './ResponseSection.module.scss';
+import { responseSelector } from '@/redux';
 import { useTranslations } from 'next-intl';
+import { useSelector } from 'react-redux';
+import styles from './ResponseSection.module.scss';
 
 export const ResponseSection = ({ isMobile }: { isMobile?: boolean }): JSX.Element => {
   const t = useTranslations('Playground');
 
+  const state = useSelector(responseSelector);
+
   return (
     <section className={styles.section}>
       {isMobile && <button className={styles.button}>{t('response')}</button>}
-      <textarea className={styles.textarea} autoCorrect="off" spellCheck={false} disabled />
+      <textarea
+        className={styles.textarea}
+        autoCorrect="off"
+        spellCheck={false}
+        disabled
+        value={state}
+      />
     </section>
   );
 };
