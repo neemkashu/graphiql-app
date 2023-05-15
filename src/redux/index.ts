a8fb7b8d33475d582029c9e11bc7506128819f76
Aliaksei Rudak <lexarudak@gmail.com>
2023-05-15 12:54:57 +0600

    fix: remove proveder from index exports

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,7 +1,6 @@
 export * from './auth';
 export * from './hooks';
 export * from './playground';
-export * from './provider';
 export * from './store';
 export * from './selectors';
 export * from './types';
