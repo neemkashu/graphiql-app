52542bc6893dd1da0aab4e3cf885e92c85f62763
neemkashu <sparjinkust@mail.ru>
2023-05-20 14:27:23 +0300

    feat: add translation in welcome page

--- src/components/welcome/Features/Features.tsx
+++ src/components/welcome/Features/Features.tsx
@@ -16,19 +16,19 @@ export const Features = (): JSX.Element => {
       <h2 className={styles.header}>{t('listHeader')}</h2>
       <ul className={styles.ul}>
         <li>
-          <h3>✨ {t('syntax')}</h3>
+          <h3>{t('syntax')}</h3>
           <div className={styles.imgWrapper}>
             <Image src={syntaxPicture} alt="" quality={90} priority className={styles.img} />
           </div>
         </li>
         <li>
-          <h3>✨ {t('errors')}</h3>
+          <h3>{t('errors')}</h3>
           <div className={styles.imgWrapper}>
             <Image src={errorPicture} alt="" quality={90} priority className={styles.img} />
           </div>
         </li>
         <li>
-          <h3>✨ {t('design')}</h3>
+          <h3>{t('design')}</h3>
           <div className={styles.imgWrapper}>
             <Image src={mobilePicture} alt="" quality={90} priority className={styles.img} />
           </div>
