4ad4033b63d6990d29f02c36d5d62c60534da3e9
Aliaksei Rudak <lexarudak@gmail.com>
2023-05-22 13:09:02 +0600

    feat: remove playground size saves

--- src/components/layout/playgroundLayout/DesktopPlayground/DesktopPlayground.tsx
+++ src/components/layout/playgroundLayout/DesktopPlayground/DesktopPlayground.tsx
@@ -13,7 +13,7 @@ import {
 import { Runner } from '@/components/Runner/Runner';
 import classNames from 'classnames';
 import { useTranslations } from 'next-intl';
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useState } from 'react';
 import SplitPane, { Pane, SashContent } from 'split-pane-react';
 import 'split-pane-react/esm/themes/default.css';
 import styles from './DesktopPlayground.module.scss';
@@ -24,50 +24,50 @@ const sashRender = (_: number, active: boolean): ReactNode => (
 
 export const DesktopPlayground = ({ children }: MultipleChildren): JSX.Element => {
   const [documentation, operation, response] = children;
-  const [sizes, setSizes] = useFieldSize<PlaygroundSize>(
-    DEFAULT_PLAYGROUND_SIZE,
-    LS_KEYS.DESKTOP_PLAYGROUND_SIZE
-  );
-  const [leftPaneSize] = sizes;
+  const [isDocs, setIsDocs] = useState(false);
+  const [sizes, setSizes] = useState([0, 9999, 9999]);
 
   const toggleLeftPane = (): void => {
-    setSizes(leftPaneSize > HIDE_PANE_SIZE ? HIDE_PANE_PLAYGROUND_SIZE : DEFAULT_PLAYGROUND_SIZE);
+    setIsDocs(!isDocs);
+    setSizes(isDocs ? [0, 9999, 9999] : [9999, 9999, 9999]);
   };
   const t = useTranslations('Playground');
 
   return (
-    <SplitPane
-      split={FieldSplit.vertical}
-      sizes={sizes}
-      onChange={setSizes}
-      sashRender={sashRender}
-    >
-      <Pane minSize={MIN_PANE_SIZE}>
-        <div className={classNames(styles.pane, styles.paneLeft)}>
-          <button onClick={toggleLeftPane} className={styles.hideButton}>
-            {leftPaneSize > HIDE_PANE_SIZE ? HIDE_BTN_ICON : SHOW_BTN_ICON}
-          </button>
-          <div className={styles.docsSection}>
-            <h4 className={styles.sectionTitle}>{t('documentation')}</h4>
-            {documentation}
+    <>
+      <button onClick={toggleLeftPane} className={styles.hideButton}>
+        {isDocs ? HIDE_BTN_ICON : SHOW_BTN_ICON}
+      </button>
+      <SplitPane
+        split={FieldSplit.vertical}
+        sizes={sizes}
+        onChange={setSizes}
+        sashRender={sashRender}
+      >
+        <Pane minSize={isDocs ? MIN_PANE_SIZE : 0} maxSize={isDocs ? 9999 : 0}>
+          <div className={classNames(styles.pane, styles.paneLeft)}>
+            <div className={styles.docsSection}>
+              <h4 className={styles.sectionTitle}>{t('documentation')}</h4>
+              {documentation}
+            </div>
           </div>
-        </div>
-      </Pane>
-      <Pane minSize={MIN_PANE_SIZE}>
-        <div className={classNames(styles.pane, styles.paneCenter)}>
-          <div className={styles.centerHeader}>
-            <h4 className={styles.sectionTitle}>{t('operation')}</h4>
-            <Runner />
+        </Pane>
+        <Pane minSize={MIN_PANE_SIZE}>
+          <div className={classNames(styles.pane, styles.paneCenter)}>
+            <div className={styles.centerHeader}>
+              <h4 className={styles.sectionTitle}>{t('operation')}</h4>
+              <Runner />
+            </div>
+            {operation}
           </div>
-          {operation}
-        </div>
-      </Pane>
-      <Pane minSize={MIN_PANE_SIZE}>
-        <div className={styles.pane}>
-          <h4 className={styles.sectionTitle}>{t('response')}</h4>
-          {response}
-        </div>
-      </Pane>
-    </SplitPane>
+        </Pane>
+        <Pane minSize={MIN_PANE_SIZE}>
+          <div className={styles.pane}>
+            <h4 className={styles.sectionTitle}>{t('response')}</h4>
+            {response}
+          </div>
+        </Pane>
+      </SplitPane>
+    </>
   );
 };
