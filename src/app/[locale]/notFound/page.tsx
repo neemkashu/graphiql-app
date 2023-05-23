b0e69b1cd18ad51d5fec3e62ba577b0d5df0c005
Aliaksei Rudak <lexarudak@gmail.com>
2023-05-23 12:46:07 +0600

    feat: add not found page animation

--- src/app/[locale]/notFound/page.tsx
+++ src/app/[locale]/notFound/page.tsx
@@ -4,20 +4,24 @@ import styles from './page.module.scss';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useTranslations } from 'next-intl';
+import { useState } from 'react';
+import classNames from 'classnames';
 
 export default function CustomNotFoundPage(): JSX.Element {
   const t = useTranslations('NotFound');
+  const [isImgLoaded, setIsImgLoaded] = useState(false);
 
   return (
     <section className={styles.page}>
       <div className={styles.imgWrapper}>
         <Image
+          className={classNames(styles.img, isImgLoaded ? styles.active : null)}
           src={'/img/not-found.png'}
           alt="not found"
           width={500}
           height={330}
           priority
-          className={`${styles.img}`}
+          onLoadingComplete={() => setIsImgLoaded(true)}
         />
       </div>
       <h1 className={styles.title}>{t('title')}</h1>
