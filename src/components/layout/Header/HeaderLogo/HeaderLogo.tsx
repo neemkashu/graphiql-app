f469f0b819606e9891e62ce85eac1bb098f80a1b
neemkashu <sparjinkust@mail.ru>
2023-05-25 12:08:16 +0300

    refactor: fix logo title

--- src/components/layout/Header/HeaderLogo/HeaderLogo.tsx
+++ src/components/layout/Header/HeaderLogo/HeaderLogo.tsx
@@ -4,8 +4,8 @@ import Image from 'next/image';
 
 export const HeaderLogo = (): JSX.Element => (
   <>
-    <span className={styles.mainText}>GraphQL |</span>
-    <span className={styles.secondText}> by No panic</span>
+    <span className={styles.mainText}>GraphiQL |</span>
+    <span className={styles.secondText}> by No Panic</span>
     <Image src={LOGO_PATH} alt={LOGO_ALT} width={30} height={30} className={styles.logo} />
   </>
 );
